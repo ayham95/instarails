76cd4f2a29246db705fb2b27d6aa4148c6c5d272
Ayham Orfali <orfali.ayham@gmail.com>
2017-08-16 16:35:32 +0300

    added post page and edit action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse
   end
 
   def new
@@ -11,6 +11,20 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(post_params)
     redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   private def post_params
